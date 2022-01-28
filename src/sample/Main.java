fb68ca1e6d0b5472665e5fbdc23f4b93a634c297
RomanStalmakov <roman_stalmakov@mail.ru>
2022-01-28 18:52:06 +0300

    update PasswordMaker.java update sample.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,21 +3,37 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.InputStream;
+
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Hello. Here's password maker!");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(400);
+
+        InputStream iconStream =
+        getClass().getResourceAsStream("/images/someImage.png");
+        Image image = new Image(iconStream);
+        primaryStage.getIcons().add(image);
+
+        Button buttonOK = new Button();
+        Button buttonCancel = new Button();
+
+
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
